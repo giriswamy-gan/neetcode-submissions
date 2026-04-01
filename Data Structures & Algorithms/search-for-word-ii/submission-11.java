07c689e5c5b197b36559c1179918265a227e257a
Ganesh G <61371990+giriswamy-gan@users.noreply.github.com>
2026-04-01 11:33:20 +0530

    Add: search-for-word-ii - submission-11

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-11.java
@@ -0,0 +1,78 @@
+class Trie {
+    Trie[] children;
+    int idx;
+    int refs;
+
+    public Trie() {
+        children = new Trie[26];
+        idx = -1;
+        refs = 0;
+    }
+
+    public void insert(String word, int i) {
+        Trie root = this;
+        root.refs++;
+        for(char ch : word.toCharArray()) {
+            int idx = ch - 'a';
+            if(root.children[idx] == null) {
+                root.children[idx] = new Trie();
+            }
+            root = root.children[idx];
+            root.refs++;
+        }
+        root.idx = i;
+    }
+}
+
+class Solution {
+    int ROWS;
+    int COLS;
+    List<String> res;
+    public List<String> findWords(char[][] board, String[] words) {
+        ROWS = board.length;
+        COLS = board[0].length;
+        Trie root = new Trie();
+        for(int i = 0; i < words.length; ++i) {
+            root.insert(words[i], i);
+        }
+
+        res = new ArrayList<>();
+        for(int r = 0; r < ROWS; ++r) {
+            for(int c = 0; c < COLS; ++c) {
+                dfs(r, c, board, words, root);
+            }
+        }
+        
+        return res;
+    }
+
+    private void dfs(int r, int c, char[][] board, String[] words, Trie root) {
+        if(Math.min(r, c) < 0 || r >= ROWS || c >= COLS || board[r][c] == '#' 
+        || root.children[board[r][c] - 'a'] == null) {
+            return;
+        }
+
+        Trie prev = root;
+        root = root.children[board[r][c] - 'a'];
+        if(root.idx != -1) {
+            res.add(words[root.idx]);
+            root.idx = -1;
+            root.refs--;
+            if(root.refs == 0) {
+                root = null;
+                prev.children[board[r][c] - 'a'] = null;
+                return;
+            }
+        }
+
+        char ch = board[r][c];
+        board[r][c] = '#';
+
+        dfs(r + 1, c, board, words, root);
+        dfs(r - 1, c, board, words, root);
+        dfs(r, c + 1, board, words, root);
+        dfs(r, c - 1, board, words, root);
+
+        board[r][c] = ch;
+    }
+}
