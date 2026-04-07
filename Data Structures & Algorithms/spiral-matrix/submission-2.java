7e96c74d8933f85dc85fb4ff25467430d7b49819
Ganesh G <61371990+giriswamy-gan@users.noreply.github.com>
2026-04-07 20:37:45 +0530

    Add: spiral-matrix - submission-2

---
+++ Data Structures & Algorithms/spiral-matrix/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int l = 0, r = matrix[0].length - 1;
+        int top = 0, bottom = matrix.length - 1;
+        List<Integer> res = new ArrayList<>();
+        while(l <= r && top <= bottom) {
+            for(int i = l; i <= r; ++i) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+            for(int i = top; i <= bottom; ++i) {
+                res.add(matrix[i][r]);
+            }
+            r--;
+            if(!(l <= r && top <= bottom)) break;
+            for(int i = r; i >= l; --i) {
+                res.add(matrix[bottom][i]);
+            }
+            bottom--;
+            for(int i = bottom; i >= top; --i) {
+                res.add(matrix[i][l]);
+            }
+            l++;
+
+        }
+        return res;
+    }
+}
