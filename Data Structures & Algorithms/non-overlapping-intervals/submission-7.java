9c915aa41c53f2652899cda0b7ebbd2bf5826d20
Ganesh G <61371990+giriswamy-gan@users.noreply.github.com>
2026-04-13 10:25:31 +0530

    Update: non-overlapping-intervals - submission-7

--- Data Structures & Algorithms/non-overlapping-intervals/submission-7.java
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-7.java
@@ -1,26 +1,16 @@
 class Solution {
-    int[] memo;
     public int eraseOverlapIntervals(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
-        memo = new int[intervals.length];
-        Arrays.fill(memo, -1);
-
-        return intervals.length - dfs(intervals, 0);
-    }
-
-    private int dfs(int[][] intervals, int i) {
-        if(i == intervals.length) return 0;
-        if(memo[i] != -1) return memo[i];
-
-        int res = 1;
-        for(int j = i + 1; j < intervals.length; ++j) {
-            if(intervals[i][1] <= intervals[j][0]) {
-                res = Math.max(res, 1 + dfs(intervals, j));
+        int prevEnd = intervals[0][1];
+        int rem = 0;
+        for(int i = 1; i < intervals.length; ++i) {
+            if(intervals[i][0] >= prevEnd) {
+                prevEnd = intervals[i][1];
+            }
+            else {
+                rem++;
             }
         }
-
-        return memo[i] = res;
+        return rem;
     }
 }
-
-// [1, 11] [2, 12] [11, 22] [1, 100]
