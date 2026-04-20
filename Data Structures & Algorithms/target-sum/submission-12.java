d7ce3936a74318c6e250e9da2e805c7b0a62bdd1
Ganesh G <61371990+giriswamy-gan@users.noreply.github.com>
2026-04-20 18:19:14 +0530

    Add: target-sum - submission-12

---
+++ Data Structures & Algorithms/target-sum/submission-12.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+        Map<Integer, Integer>[] map = new HashMap[n + 1];
+        for(int i = 0; i <= n; ++i) {
+            map[i] = new HashMap<>();
+        }
+        map[0].put(0, 1);
+
+        for(int i = 0; i < n; ++i) {
+            for(Map.Entry<Integer, Integer> entry : map[i].entrySet()) {
+                int total = entry.getKey();
+                int count = entry.getValue();
+                map[i + 1].put(total + nums[i], map[i + 1].getOrDefault(total + nums[i], 0) + count);
+                map[i + 1].put(total - nums[i], map[i + 1].getOrDefault(total - nums[i], 0) + count);
+            }
+        }
+
+        return map[n].getOrDefault(target, 0);
+    }
+}
