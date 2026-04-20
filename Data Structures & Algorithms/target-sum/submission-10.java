ac6cada52ef99026e8d392efea6c26235a99f732
Ganesh G <61371990+giriswamy-gan@users.noreply.github.com>
2026-04-20 18:04:37 +0530

    Add: target-sum - submission-10

---
+++ Data Structures & Algorithms/target-sum/submission-10.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        return dfs(0, nums, target);
+    }
+
+    private int dfs(int i, int[] nums, int target) {
+        if(i == nums.length) return target == 0 ? 1 : 0;
+
+        return dfs(i + 1, nums, target + nums[i]) + dfs(i + 1, nums, target - nums[i]);
+    }
+}
