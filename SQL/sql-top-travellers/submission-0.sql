5ec68e08a77b75806d1fe85c620944e02ddd711d
Ganesh G <61371990+giriswamy-gan@users.noreply.github.com>
2026-04-27 19:11:12 +0530

    Add: sql-top-travellers - submission-0

---
+++ SQL/sql-top-travellers/submission-0.sql
@@ -0,0 +1,7 @@
+-- Write your query below
+select u.name, 
+SUM(case when r.distance is null then 0 else r.distance end) as travelled_distance
+from users u left join rides r
+on u.id = r.user_id
+group by u.id
+order by travelled_distance desc, u.name;
