e7013d65e019bda22497d4286141e0fd3b33758c
Ganesh G <61371990+giriswamy-gan@users.noreply.github.com>
2026-05-01 11:27:19 +0530

    Add: valid-parenthesis-string - submission-8

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-8.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int leftMin = 0;
+        int leftMax = 0;
+        for(char ch : s.toCharArray()) {
+            if(ch == '(') {
+                leftMin++;
+                leftMax++;
+            }
+            else if(ch == ')') {
+                leftMin--;
+                leftMax--;
+            }
+            else {
+                leftMin--;
+                leftMax++;
+            }
+            if(leftMin < 0) {
+                leftMin = 0;
+            }
+            if(leftMax < 0) {
+                return false;
+            }
+        }
+        return leftMin == 0;
+    }
+}
