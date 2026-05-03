a4cdf6e3f14434cb94fc2c4f544a7b84d19194b4
Ganesh G <61371990+giriswamy-gan@users.noreply.github.com>
2026-05-03 11:34:05 +0530

    Add: reverse-bits - submission-4

---
+++ Data Structures & Algorithms/reverse-bits/submission-4.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        for(int i = 0; i < 32; ++i) {
+            if((n & 1) == 1) {
+                res |= 1 << (31 - i);
+            }
+            n = n >> 1;
+        }
+        return res;
+    }
+}
